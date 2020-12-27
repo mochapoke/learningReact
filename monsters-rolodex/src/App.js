df065d487eabc956345659f974a38c4892ac4fe7
mocha <syda07@gmail.com>
2020-12-28 08:16:50 +0900

    리팩토링: handleChange

--- monsters-rolodex/src/App.js
+++ monsters-rolodex/src/App.js
@@ -11,12 +11,18 @@ class App extends Component {
       monsters: [],
       searchField: ''
     }
+
+    this.handleChange = this.handleChange.bind(this);
   }
 
   componentDidMount(){
     fetch('https://jsonplaceholder.typicode.com/users')
     .then(response => response.json())
     .then(users => this.setState({monsters: users}))
+  }
+
+  handleChange(e){
+    this.setState({searchField: e.target.value})
   }
 
   render(){
@@ -30,7 +36,7 @@ class App extends Component {
       <div className="App">
         <SearchBox 
           placeholder='search monsters' 
-          handleChange={e => this.setState({searchField: e.target.value})}
+          handleChange={this.handleChange}
         />
         <CardList monsters={filteredMonsters}></CardList>
       </div>
