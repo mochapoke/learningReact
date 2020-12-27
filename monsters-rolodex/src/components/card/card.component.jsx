7c4e59f7632367778de9f095b2fc548feb985978
mocha <syda07@gmail.com>
2020-12-28 03:58:44 +0900

    추가: 검색 기능

--- monsters-rolodex/src/components/card/card.component.jsx
+++ monsters-rolodex/src/components/card/card.component.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import './card.styles.css'
 
-export const Card = (props) => (
+export const Card = props => (
     <div className='card-container'>
       <img alt='monsters' src={`https://robohash.org/${props.monster.id}?set=set4&size=180x180`} />
       <h2> {props.monster.name} </h2>
