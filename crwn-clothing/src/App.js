034a17edb3ebba8c35b804d4b6c0a9e4d22d846a
mocha <syda07@gmail.com>
2021-01-03 14:17:52 +0900

    ➕ react router dom

--- crwn-clothing/src/App.js
+++ crwn-clothing/src/App.js
@@ -1,11 +1,34 @@
 import React from 'react';
+import { Route, Switch, Link } from 'react-router-dom'
+
 import './App.css';
-import Homepage from './pages/homepage/homepage.component';
+import HomePage from './pages/homepage/homepage.component';
+
+const HatsPage = () => (
+  <div>
+    <Link to='/'>home</Link>
+    <h1>HATS PAGE</h1>
+  </div>
+)
+
+const itemDetail = props => {
+  console.log(props);
+  return(
+    <div>
+      <button onClick={ () => props.history.push('/hats')}>HatsPage</button>
+      <h1>HATS DETAIL PAGE: {props.match.params.itemID} </h1>
+    </div>
+  )
+}
 
 function App() {
   return (
     <div>
-      <Homepage />
+      <Switch>
+        <Route exact path='/' component={HomePage} />
+        <Route exact path='/hats' component={HatsPage} />
+        <Route path='/hats/:itemID' component={itemDetail} />
+      </Switch>
     </div>
   );
 }
