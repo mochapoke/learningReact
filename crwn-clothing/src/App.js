da99931ab350888a5c1703944adb1b8d19800070
mocha <syda07@gmail.com>
2021-01-04 15:29:10 +0900

    ➕ shopPage 구성

--- crwn-clothing/src/App.js
+++ crwn-clothing/src/App.js
@@ -2,7 +2,9 @@ import React from 'react';
 import { Route, Switch, Link } from 'react-router-dom'
 
 import './App.css';
+
 import HomePage from './pages/homepage/homepage.component';
+import ShopPage from './pages/shop/shop.component';
 
 const HatsPage = () => (
   <div>
@@ -27,6 +29,7 @@ function App() {
       <Switch>
         <Route exact path='/' component={HomePage} />
         <Route exact path='/hats' component={HatsPage} />
+        <Route exact path='/shop' component={ShopPage} />
         <Route path='/hats/:itemID' component={itemDetail} />
       </Switch>
     </div>
