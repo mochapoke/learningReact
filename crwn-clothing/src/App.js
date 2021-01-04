08d5861f819926459e272d0413e7cea56573e0df
mocha <syda07@gmail.com>
2021-01-04 17:24:37 +0900

    ➕ header components and links

--- crwn-clothing/src/App.js
+++ crwn-clothing/src/App.js
@@ -5,6 +5,7 @@ import './App.css';
 
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
+import Header from './components/header/header.component'
 
 const HatsPage = () => (
   <div>
@@ -26,6 +27,7 @@ const itemDetail = props => {
 function App() {
   return (
     <div>
+      <Header />
       <Switch>
         <Route exact path='/' component={HomePage} />
         <Route exact path='/hats' component={HatsPage} />
