60a1da2ae0090c64575d3d26c2d29cacf464e8e1
mocha <syda07@gmail.com>
2021-01-06 17:54:16 +0900

    ➕ 웹가입시 유저정보 firestore에 저장

--- crwn-clothing/src/App.js
+++ crwn-clothing/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Route, Switch, Link } from 'react-router-dom'
+import { Route, Switch } from 'react-router-dom'
 
 import './App.css';
 
@@ -7,7 +7,7 @@ import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
 import Header from './components/header/header.component'
 import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component'
-import { auth } from './firebase/firebase.utils'
+import { auth, createUserProfileDocument } from './firebase/firebase.utils'
 
 
 class App extends React.Component{
@@ -22,9 +22,10 @@ class App extends React.Component{
   unsubscribeFromAuth = null;
 
   componentDidMount(){
-    this.unsubscribeFromAuth = auth.onAuthStateChanged(user => {
-      this.setState({currentUser: user});
-      console.log(user);
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(async user => {
+      // this.setState({currentUser: user});
+      createUserProfileDocument(user);
+    
     })
   }
 
