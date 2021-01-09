b5de66f0bbdbb89ed538d2fd13ce71bf16348a35
mocha <syda07@gmail.com>
2021-01-09 18:05:02 +0900

    💄 shop page

--- crwn-clothing/src/components/collection-item/collection-item.component.jsx
+++ crwn-clothing/src/components/collection-item/collection-item.component.jsx
@@ -1,20 +1,23 @@
 import React from 'react';
 
+import CustomButton from '../custom-button/custom-button.component';
+
 import './collection-item.styles.scss';
 
-const CollectionItem = ({id, name, price, imageUrl}) => (
+const CollectionItem = ({ id, name, price, imageUrl }) => (
   <div className='collection-item'>
-    <div 
+    <div
       className='image'
       style={{
-        backgroundImage: `url(${imageUrl})`
-      }}  
+        backgroundImage: `url(${imageUrl})`,
+      }}
     />
     <div className='collection-footer'>
       <span className='name'>{name}</span>
       <span className='price'>{price}</span>
     </div>
+    <CustomButton inverted>ADD TO CART</CustomButton>
   </div>
-)
+);
 
-export default CollectionItem
+export default CollectionItem;
