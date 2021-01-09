07b48bf42756aed2d5510558ada5cb42c2216d87
mocha <syda07@gmail.com>
2021-01-09 18:42:21 +0900

    ➕ cart에 아이템 추가 기능 구현(redux)

--- crwn-clothing/src/components/collection-preview/collection-preview.component.jsx
+++ crwn-clothing/src/components/collection-preview/collection-preview.component.jsx
@@ -1,22 +1,19 @@
 import React from 'react';
 import CollectionItem from '../collection-item/collection-item.component';
 
-
 import './collection-preview.styles.scss';
 
-const CollectionPreview = ({title, items, id}) => (
+const CollectionPreview = ({ title, items, id }) => (
   <div className='collection-preview'>
     <h1 className='title'>{title.toUpperCase()}</h1>
     <div className='preview'>
-      {
-        items
-        .filter((item,index) => index < 4)
-        .map(({id, ...otherItemProps}) => (
-          <CollectionItem key={id} {...otherItemProps} />
-        ))
-      }
+      {items
+        .filter((item, index) => index < 4)
+        .map((item) => (
+          <CollectionItem key={item.id} item={item} />
+        ))}
     </div>
   </div>
-)
+);
 
 export default CollectionPreview;
