7af017d96f30f99888e5eb45a3c3a0e8c876a610
mocha <syda07@gmail.com>
2021-01-12 02:18:16 +0900

    collection Routing and Selector

--- crwn-clothing/src/pages/collection/collection.component.jsx
+++ crwn-clothing/src/pages/collection/collection.component.jsx
@@ -1,15 +1,25 @@
 import React from 'react';
 
+import { connect } from 'react-redux';
+
+// import CollectionItem from '../../components/collection-item/collection-item.component';
+
+import { selectCollection } from '../../redux/shop/shop.selectors';
+
 import './collection.styles.scss';
 
-const CollectionPage = ({ match }) => {
-  console.log(match.params.collectionID);
+const CollectionPage = ({ collection }) => {
+  console.log(collection);
 
   return (
     <div className='collection'>
-      <h2>COLLECTION PAGE: {match.params.collectionID}</h2>
+      <h2>COLLECTION PAGE: </h2>
     </div>
   );
 };
 
-export default CollectionPage;
+const mapStateToProps = (state, ownProps) => ({
+  collection: selectCollection(ownProps.match.params.collectionID)(state),
+});
+
+export default connect(mapStateToProps)(CollectionPage);
