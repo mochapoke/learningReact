2e85bdfb9803edc86e674beb2b9af4a7cda9fed6
mocha <syda07@gmail.com>
2021-01-11 17:12:50 +0900

    🏗: Directory State 를 Redux.reducer로 관리하게 수정

--- crwn-clothing/src/components/directory/directory.component.jsx
+++ crwn-clothing/src/components/directory/directory.component.jsx
@@ -1,28 +1,22 @@
 import React from 'react';
-import MenuItem from '../menu-item/menu-item.component'
-import SECTIONS_DATA from '../../sections.data';
+import { connect } from 'react-redux';
+import { createStructuredSelector } from 'reselect';
+
+import { selectDirectorySections } from '../../redux/directory/directory.selectors';
+
+import MenuItem from '../menu-item/menu-item.component';
 import './directory.styles.scss';
 
-class Directory extends React.Component {
-  constructor() {
-    super();
+const Directory = ({ sections }) => (
+  <div className='directory-menu'>
+    {sections.map(({ id, ...otherSectionProps }) => (
+      <MenuItem key={id} {...otherSectionProps} />
+    ))}
+  </div>
+);
 
-    this.state = {
-      sections: SECTIONS_DATA
-    }
-  }
-  
-  render(){
-    return(
-      <div className='directory-menu'>
-        {
-          this.state.sections.map(({id, ...otherSectionProps}) => (
-            <MenuItem key={id} {...otherSectionProps} />
-          ))
-        }
-      </div>
-    )
-  }
-}
+const mapStateToProps = createStructuredSelector({
+  sections: selectDirectorySections,
+});
 
-export default Directory;
+export default connect(mapStateToProps)(Directory);
---
+++ crwn-clothing/src/redux/directory/directory.selectors.js
@@ -0,0 +1,9 @@
+import { createSelectorHook } from 'react-redux';
+import { createSelector } from 'reselect';
+
+const selectDirectory = (state) => state.directory;
+
+export const selectDirectorySections = createSelector(
+  [selectDirectory],
+  (directory) => directory.sections
+);
