6d24d3bba56c1fb901e9dc8035c813a3e918642e
mocha <syda07@gmail.com>
2021-01-12 00:27:06 +0900

    🏗:✨route로 category Page 연결

---
+++ crwn-clothing/src/pages/category/category.component.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import './category.styles.scss';
+
+const CategoryPage = ({ match }) => {
+  console.log(match.params.categoryID);
+  return (
+    <div className='category'>
+      <h2>CATEGORY PAGE</h2>
+    </div>
+  );
+};
+
+export default CategoryPage;
