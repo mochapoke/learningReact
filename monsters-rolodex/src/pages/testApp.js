d1611654f2d23e98cd87406846ea83cf92dbafbc
Mocha <syda07@gmail.com>
2021-01-19 22:54:03 +0900

    🐛 첫화면이 공백인거 고침

--- monsters-rolodex/src/pages/testApp.js
+++ monsters-rolodex/src/pages/testApp.js
@@ -1,39 +1,45 @@
 import React, { useEffect, useState } from 'react';
+import '../App.css';
 
 import { CardList } from '../components/card-list/card-list.component';
 import { SearchBox } from '../components/search-box/search-box.component';
 
 const TestApp = () => {
   const [monsters, setMonsters] = useState([]);
-  const [searchField, setSearchField] = useState('');
+  const [searchTerm, setSearchTerm] = useState('');
   const [filteredResult, setFilteredResult] = useState([]);
 
-  const fetchItems = async () => {
-    const response = await fetch('https://jsonplaceholder.typicode.com/users');
-    const data = await response.json();
-    setMonsters(data);
+  const handleChange = (event) => {
+    setSearchTerm(event.target.value);
+
+    const filteredMonsters = monsters.filter((monster) =>
+      monster.name.toLowerCase().includes(searchTerm.toLowerCase())
+    );
+
+    setFilteredResult(filteredMonsters);
   };
 
   useEffect(() => {
+    const fetchItems = async () => {
+      const response = await fetch(
+        'https://jsonplaceholder.typicode.com/users'
+      );
+      const data = await response.json();
+      setMonsters(data);
+    };
     fetchItems();
   }, []);
-
-  useEffect(() => {
-    const results = monsters.filter((monster) =>
-      monster.name.toLowerCase().includes(searchField.toLowerCase())
-    );
-
-    setFilteredResult(results);
-  }, [searchField]);
 
   return (
     <div className='App'>
       <h1>Monster Roledex</h1>
-      <SearchBox
-        placeholder='search monsters'
-        handleChange={(event) => setSearchField(event.target.value)}
-      />
-      <CardList monsters={filteredResult}></CardList>
+      <SearchBox placeholderw='search monsters' handleChange={handleChange} />
+
+      {!searchTerm ? (
+        <CardList monsters={monsters}></CardList>
+      ) : (
+        <CardList monsters={filteredResult}></CardList>
+      )}
     </div>
   );
 };
