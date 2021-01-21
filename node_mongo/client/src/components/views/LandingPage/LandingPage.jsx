8fbd561997a08544ee11f855d1c9bdfcd74d817a
Mocha <syda07@gmail.com>
2021-01-21 19:41:40 +0900

    ✨ LOG - OUT feature!

--- node_mongo/client/src/components/views/LandingPage/LandingPage.jsx
+++ node_mongo/client/src/components/views/LandingPage/LandingPage.jsx
@@ -1,12 +1,38 @@
 import React, { useEffect } from 'react';
 import axios from 'axios';
 
-const LandingPage = () => {
+const LandingPage = (props) => {
   useEffect(() => {
     axios.get('/api/hello').then((res) => console.log(res.data));
   }, []);
 
-  return <div>LandingPage</div>;
+  const onClickHandler = () => {
+    axios.get('/api/users/logout').then((res) => {
+      //      console.log(res.data);
+      if (res.data.success) {
+        props.history.push('/login');
+      } else {
+        alert('로그아웃하는데 실패함');
+      }
+    });
+  };
+
+  return (
+    <div
+      style={{
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        alignContent: 'center',
+        justifyContent: 'center',
+        width: '100%',
+        height: '100vh',
+      }}
+    >
+      <h2>Landing Page</h2>
+      <button onClick={onClickHandler}>Log Oot</button>
+    </div>
+  );
 };
 
 export default LandingPage;
--- node_mongo/server/models/User.js
+++ node_mongo/server/models/User.js
@@ -81,6 +81,6 @@ userSchema.statics.findByToken = function (token, cb) {
     });
   });
 };
-const User = mongoose.model('User', userSchema);
 
+const User = mongoose.model('User', userSchema);
 module.exports = { User };
